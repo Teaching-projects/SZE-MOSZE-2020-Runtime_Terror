82b2e8a2ce46064485d25f765bbb237a562c9f32
tamastomordi <tomordi.tamas@gmail.com>
2020-09-16 11:01:50 +0200

    Remove classes from main.cpp

--- main.cpp
+++ main.cpp
@@ -3,77 +3,10 @@
 #include <time.h>
 #include <vector>
 
+#include "Unit.h"
+#include "Game.h"
+
 using namespace std;
-
-class Unit {
-
-public:
-    string name;
-    int health{};
-    int damage{};
-
-    bool IsDead() {
-        return (health <= 0) ? true : false;
-    }
-
-    Unit() {
-
-    }
-
-    Unit(string name, int health, int damage) {
-        this->name = name;
-        this->health = health;
-        this->damage = damage;
-    }
-
-    void Attack(Unit &target) {
-        target.health -= this->damage;
-        if (target.health < 0) target.health = 0;
-    }
-};
-
-class Game {
-
-public:
-    Unit player;
-    Unit enemy;
-
-    Game(vector<string> data) {
-        this->player = Unit(data[1], stoi(data[2]), stoi(data[3]));
-        this->enemy = Unit(data[4], stoi(data[5]), stoi(data[6]));
-    }
-
-    void SimulateTurn(Unit &attacker, Unit &target) {          
-        cout << attacker.name << " -> " << target.name << endl;
-        attacker.Attack(target);
-        PrintCurrentState();             
-    }
-
-    void Combat() {
-        PrintCurrentState();
-        bool endCombat = false;
-        while (!endCombat) {
-            SimulateTurn(player, enemy);
-            if (enemy.IsDead()) {
-                endCombat = true;
-                cout << enemy.name << " died. " << player.name << " wins." << endl;
-                break;
-            }  
-            SimulateTurn(enemy, player);    
-            if (player.IsDead()) {
-                endCombat = true;
-                cout << player.name << "died. " << enemy.name << " wins." << endl;
-                break;
-            }
-        }
-    }
-
-    void PrintCurrentState() {
-        cout << player.name << ": " << "HP: " << player.health << ", DMG: " << player.damage << endl;
-        cout << enemy.name << ": " << "HP: " << enemy.health << ", DMG: " << enemy.damage << endl;
-    }
-
-};
 
 int main(int argc, char* argv[])
 {
@@ -82,6 +15,9 @@ int main(int argc, char* argv[])
         data.push_back(argv[i]);
     }        
 
-    Game game = Game(data);
+    Unit* player = new Unit(data[1], stoi(data[2]), stoi(data[3]));
+    Unit* enemy = new Unit(data[4], stoi(data[5]), stoi(data[6]));
+
+    Game game = Game(*player, *enemy);
     game.Combat();
 }
