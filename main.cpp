7882016067c5ab24ca24b47a4a52e4f202fb715f
jabezare <jabe0212@gmail.com>
2020-09-16 12:54:45 +0200

    Removed overcomplication from main

--- main.cpp
+++ main.cpp
@@ -9,14 +9,9 @@
 using namespace std;
 
 int main(int argc, char* argv[])
-{
-    vector<string> data;
-    for (int i = 0; i < argc; ++i) {
-        data.push_back(argv[i]);
-    }        
-
-    Unit* player = new Unit(data[1], stoi(data[2]), stoi(data[3]));
-    Unit* enemy = new Unit(data[4], stoi(data[5]), stoi(data[6]));
+{ 
+    Unit* player = new Unit(argv[1], stoi(argv[2]), stoi(argv[3]));
+    Unit* enemy = new Unit(argv[4], stoi(argv[5]), stoi(argv[6]));
 
     Game game = Game(*player, *enemy);
     game.Combat();
