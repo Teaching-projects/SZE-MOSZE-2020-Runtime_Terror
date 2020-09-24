0d7b9a98d47d511a6a7eb25bef910bc2505e2a64
tamastomordi <tomordi.tamas@gmail.com>
2020-09-24 21:24:42 +0200

    Return 1 in case of an error

--- main.cpp
+++ main.cpp
@@ -14,7 +14,9 @@ int main(int argc, char *argv[])
         Unit a = Unit::parseUnit("unit/"+string(argv[1]));
         Unit b = Unit::parseUnit("unit/"+string(argv[2]));
         Game game = Game(a, b);
+        return 0;
     } catch(const std::runtime_error& e) {
         std::cerr << "exception: " << e.what() << std::endl; 
+        return 1;
     }
 }
