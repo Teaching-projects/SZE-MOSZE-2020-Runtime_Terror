4328d0ee39d3785d8001d5590997413d28b7a0cb
tamastomordi <tomordi.tamas@gmail.com>
2020-10-29 11:05:40 +0100

    Add doxygen comments to Json class

--- Json.h
+++ Json.h
@@ -1,14 +1,48 @@
+/**
+ * \class Json
+ * 
+ * \brief Json Parser class containing static functions
+ * 
+ * \author Tömördi Tamás
+ * 
+ * Created on: 2020/10/29 10:56
+*/
+
 #include <map>
 #include <string>
 
 class Json
 {
 private:
-    static void Validator(const std::string &text);
-    static std::string Trim(const std::string &text);
+
+    /**
+     * \brief Checks whether a string is a valid json or not
+    */
+    static void Validator(const std::string &text /** [in] The text to be validated*/);
+    
+    /**
+     * \brief Removes unnecessary space characters from a string
+     * \return A string without spaces
+    */
+    static std::string Trim(const std::string &text /** [in] The text to be trimmed*/);
 
 public:
-    static std::map<std::string, std::string> ParseString(const std::string &input);
-    static std::map<std::string, std::string> ParseStream(std::istream &stream);
-    static std::map<std::string, std::string> ParseFile(const std::string &filename);
+
+    /**
+     * \brief Parses data from string
+     * \return A map of the data: key and value pairs
+    */
+    static std::map<std::string, std::string> ParseString(const std::string &input /** [in] The string to be parsed*/);
+
+    /**
+     * \brief Parses data from stream
+     * \return A map of the data: key and value pairs
+    */
+    static std::map<std::string, std::string> ParseStream(std::istream &stream /** [in] The stream to be parsed*/);
+
+    /**
+     * \brief Parses data from file
+     * \return A map of the data: key and value pairs
+    */
+    static std::map<std::string, std::string> ParseFile(const std::string &filename /** [in] The name of the file to be parsed*/);
 };
