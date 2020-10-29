6a0554e8fc2fd760620ac7141b052572dc8f1852
tamastomordi <tomordi.tamas@gmail.com>
2020-10-29 10:23:08 +0100

    Add doxygen comments to Player class

--- Player.h
+++ Player.h
@@ -1,3 +1,13 @@
+/**
+ * \class Player
+ * 
+ * \brief Player class inherited from Unit class
+ * 
+ * \author Tömördi Tamás
+ * 
+ * Created on: 2020/10/29 10:11
+*/
+
 #ifndef PLAYER_HEADER
 #define PLAYER_HEADER
 
@@ -6,19 +16,30 @@
 class Player : public Unit 
 {
     public:
-    Player(const std::string&, const int, const int, const float);    
+    Player(const std::string&, const int, const int, const float); ///< Constructor of Player class    
 
-    int GetLevel() const;
-    int GetXp() const;
+    int GetLevel() const; ///< This is a simple getter function for level value
+    int GetXp() const; ///< This is a simple getter function for xp value
 
-    void Attack(Player&);
-    static Player parsePlayer(const std::string&);
+    /**
+     * \brief Damages target Player using the Unit's Attack function, increasing the xp and calling the LevelUp() function in case of level up
+    */
+    void Attack(Player& /** [in] The Player the attack is targeting*/);
+    
+    /**
+     * \brief Fills Player data from JSON
+     * \return Player with values from the specified JSON
+    */
+    static Player parsePlayer(const std::string& /** [in] The name of the file to be parsed*/);
 
     private:
-    int xp = 0;
-    int level = 1;
-    int maxHealth;
+    int xp = 0; ///< The experience points of a Player
+    int level = 1; ///< The level of a Player
+    int maxHealth; ///< The maximum health value of a Player
 
+    /**
+     * \brief Increasing the Player's level, maxHealth and damage; and setting the health value to maxHealth
+    */
     void LevelUp();
 };
 
