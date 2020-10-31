9b2f2a73e04ef17912872ae0a74899b36e78f94a
Jabezare <36441872+Jabezare@users.noreply.github.com>
2020-10-31 08:58:36 +0000

    Update gh-pages to output generated at 273a46e

--- search/functions_1.js
+++ search/functions_1.js
@@ -4,7 +4,5 @@ var searchData=
   ['getcurrentcooldown',['GetCurrentCooldown',['../classUnit.html#a1c60786c8cf4aed70995b5274d7d0887',1,'Unit']]],
   ['getdamage',['GetDamage',['../classUnit.html#a705ecc57256f3535b35eae4892ce8b2a',1,'Unit']]],
   ['gethealth',['GetHealth',['../classUnit.html#a7257cded6f3117bc680bf070668f3d4f',1,'Unit']]],
-  ['getlevel',['GetLevel',['../classPlayer.html#aff4ab97fc298001b64ef74c7556f4211',1,'Player']]],
-  ['getname',['GetName',['../classUnit.html#a9ddebd39e011bae985c78ba777374fb2',1,'Unit']]],
-  ['getxp',['GetXp',['../classPlayer.html#a32e324622aa95738a6e0a8c7e7b2f19e',1,'Player']]]
+  ['getname',['GetName',['../classUnit.html#a9ddebd39e011bae985c78ba777374fb2',1,'Unit']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,9 +1,4 @@
 var searchData=
 [
-  ['parsefile',['ParseFile',['../classJson.html#a94f9a0b4aae1017646d9973b5dad6374',1,'Json']]],
-  ['parseplayer',['parsePlayer',['../classPlayer.html#a42b2e4ec5ff29b8a5e73468f3eea7782',1,'Player']]],
-  ['parsestream',['ParseStream',['../classJson.html#a37214223a3672747e113d5e1cf23d3fd',1,'Json']]],
-  ['parsestring',['ParseString',['../classJson.html#ab324310036367d24b68814201fffae6a',1,'Json']]],
-  ['parseunit',['parseUnit',['../classUnit.html#a3d362944da10ba7d3bcbf7c98dd20f50',1,'Unit']]],
-  ['player',['Player',['../classPlayer.html#a66d558bd243e52a6b0281d452bf585ac',1,'Player']]]
+  ['parseunit',['parseUnit',['../classUnit.html#a3d362944da10ba7d3bcbf7c98dd20f50',1,'Unit']]]
 ];
