e11a424a79ed909d3c0a448dfee99299cfaae077
Jabezare <jabe0212@gmail.com>
2020-11-19 12:05:02 +0100

    Added defense to monsters, added getDefense()

--- Monster.h
+++ Monster.h
@@ -18,7 +18,7 @@ class Monster
 {
 
 public:
-    Monster(const std::string, const int, const int, const double); ///< Constructor of Monster class
+    Monster(const std::string, const int, const int, const double, const double); ///< Constructor of Monster class
 
     /**
      * \brief Checks if the Monster's health value is greater than zero
@@ -50,12 +50,15 @@ public:
     int getDamage() const;
     /// This is a simple getter function for current time until next attack
     double getAttackCoolDown() const;
+    /// This is a simple getter function for current defense value
+    double getDefense() const;
 
     protected:
     const std::string name; ///< The name of a unit
     int health; ///< The health value of a unit
     int damage; ///< The damage value of a unit
     double atkcooldown; ///< The time between the unit's attacks
+    double defense; ///< The defense value of a unit
 
     private:
     static void RemoveChar(std::string & /** [in] The target string the charater(s) need to be removed from */, const char /** [in] The charater to be removed*/); ///< Removes specified characters from a given string. Used by the parseMonster function
