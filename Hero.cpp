ff1f7208b27375847e5dd37015226d660e3f2b1b
Jabezare <jabe0212@gmail.com>
2020-11-19 12:06:28 +0100

    Added defense and dbpl to Hero class

--- Hero.cpp
+++ Hero.cpp
@@ -2,10 +2,10 @@
 #include "JSON.h"
 #include <iostream>
 
-Hero::Hero(const std::string &name, const int health, const int damage, const double atkcooldown,
-           const int ExperiencePerLevel, const int HealthPointBonusPerLevel, const int DamageBonusPerLevel, const double ColdownMultiplierPerLevel)
-    : Monster{name, health, damage, atkcooldown}, maxHealth(health), ExperiencePerLevel(ExperiencePerLevel), HealthPointBonusPerLevel(HealthPointBonusPerLevel),
-      DamageBonusPerLevel(DamageBonusPerLevel), ColdownMultiplierPerLevel(ColdownMultiplierPerLevel)
+Hero::Hero(const std::string &name, const int health, const int damage, const double atkcooldown, const double defense,
+           const int ExperiencePerLevel, const int HealthPointBonusPerLevel, const int DamageBonusPerLevel, const double ColdownMultiplierPerLevel, const double DefenseBonusPerLevel)
+    : Monster{name, health, damage, atkcooldown, defense}, maxHealth(health), ExperiencePerLevel(ExperiencePerLevel), HealthPointBonusPerLevel(HealthPointBonusPerLevel),
+      DamageBonusPerLevel(DamageBonusPerLevel), ColdownMultiplierPerLevel(ColdownMultiplierPerLevel), DefenseBonusPerLevel(DefenseBonusPerLevel)
 {
 }
 
@@ -57,8 +57,10 @@ Hero Hero::parse(const std::string &fileName)
         data.get<int>("base_health_points"),
         data.get<int>("base_damage"),
         data.get<double>("base_attack_cooldown"),
+        data.get<double>("defense"),
         data.get<int>("experience_per_level"),
         data.get<int>("health_point_bonus_per_level"),
         data.get<int>("damage_bonus_per_level"),
-        data.get<double>("cooldown_multiplier_per_level"));
+        data.get<double>("cooldown_multiplier_per_level"),
+        data.get<double>("defense_bonus_per_level"));
 }
