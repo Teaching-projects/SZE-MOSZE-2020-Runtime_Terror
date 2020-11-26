ce773ce03d066f8cc10150240eccc0f3fce586ff
Csillag Bence <45268852+csibe1999@users.noreply.github.com>
2020-11-26 22:01:48 +0100

    Added doxy

--- Map.h
+++ Map.h
@@ -1,3 +1,13 @@
+/**
+ * \class MAP
+ * 
+ * \brief MAP class can read a game map from text file
+ * 
+ * \author Csillag Bence
+ * 
+ * Created on: 2020/11/26 18:21
+*/
+
 #include <string>
 #include <fstream>
 #include <vector>
@@ -5,22 +15,22 @@
 class Map
 {
 private:
-    std::vector<std::string> map;
+    std::vector<std::string> map; ///< This vector store a map
 
 public:
-    Map();
-    Map(std::string &filename);
-    enum type
+    Map();                      ///< Constructor of Map class
+    Map(std::string &filename); ///< Constructor of Map class, which read map from file
+    enum type                   ///< Enum for 2 types of map element
     {
         Wall,
         Free
     };
-    Map::type get(size_t x, size_t y) const;
+    Map::type get(size_t x, size_t y) const; ///< Get a map field type
 
-    int getWidth(int line) const;
-    int getHeight() const;
+    int getWidth(int line) const; ///< This getter function return with width of map
+    int getHeight() const;        ///< This getter function return with height of map
 
-    class WrongIndexException : public std::runtime_error
+    class WrongIndexException : public std::runtime_error ///< Map class Exception function
     {
     public:
         explicit WrongIndexException(const std::string &Error) : std::runtime_error(Error) {}
