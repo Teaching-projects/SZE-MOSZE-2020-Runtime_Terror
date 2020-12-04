57cadd792ce5c9fd1e5cef052062778d6f50047b
Jabezare <jabe0212@gmail.com>
2020-12-04 09:38:51 +0100

    Restructured main, added new input/output

--- main.cpp
+++ main.cpp
@@ -29,38 +29,8 @@ void bad_exit(int exitcode)
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        bad_exit(1);
-    if (!std::filesystem::exists(argv[1]))
-        bad_exit(2);
-
-    std::string hero_file;
-    std::list<std::string> monster_files;
     try
     {
-        JSON scenario = JSON::parseFromFile(argv[1]);
-        if (!(scenario.count("hero") && scenario.count("monsters")))
-            bad_exit(3);
-        else
-        {
-            hero_file = scenario.get<std::string>("hero");
-            JSON::list monster_file_list = scenario.get<JSON::list>("monsters");
-            for (auto monster_file : monster_file_list)
-                monster_files.push_back(std::get<std::string>(monster_file));
-        }
-    }
-    catch (const JSON::ParseException &e)
-    {
-        bad_exit(4);
-    }
-
-    try
-    {
-        Hero hero{Hero::parse(hero_file)};
-        std::vector<Monster> monsters;
-        for (const auto &monster_file : monster_files)
-            monsters.push_back(Monster::parse(monster_file));
-
         PreparedGame game = PreparedGame("preparedgame.json");
         game.run();
     }
