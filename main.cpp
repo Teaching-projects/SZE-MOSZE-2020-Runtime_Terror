e5fd25c23657d868a283005090e4344e0930a4b9
Tömördi Tamás <tomordi.tamas@gmail.com>
2020-12-06 13:50:32 +0100

    Simplify and edit main

--- main.cpp
+++ main.cpp
@@ -1,42 +1,21 @@
 #include <iostream>
-#include <map>
-#include <string>
-#include <filesystem>
-#include <sstream>
-#include <algorithm>
-#include <iterator>
-#include <list>
-
 #include "JSON.h"
-#include "Hero.h"
-#include "Monster.h"
-#include "Game.h"
 #include "PreparedGame.h"
-
-const std::map<int, std::string> error_messages = {
-    {1, "Bad number of arguments. Only a single scenario file should be provided."},
-    {2, "The provided scenario file is not accessible."},
-    {3, "The provided scenario file is invalid."},
-    {4, "JSON parsing error."}};
-
-void bad_exit(int exitcode)
-{
-    std::cerr
-        << (error_messages.count(exitcode) ? error_messages.at(exitcode) : "Unknown error")
-        << std::endl;
-    exit(exitcode);
-}
+#include "HeroTextRenderer.h"
+#include "ObserverTextRenderer.h"
 
 int main(int argc, char **argv)
 {
     try
     {
         PreparedGame game = PreparedGame("preparedgame.json");
+        game.registerRenderer(new HeroTextRenderer());
         game.run();
     }
     catch (const JSON::ParseException &e)
     {
-        bad_exit(4);
+        std::cerr << "Unknown error" << std::endl;
+        return 1;
     }
     return 0;
 }
