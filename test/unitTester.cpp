53d8080c3f40c0061ab62e9b6f40232e52cc5d1b
Tömördi Tamás <tomordi.tamas@gmail.com>
2020-12-07 11:52:01 +0100

    Minor changes in unit tester

--- test/unitTester.cpp
+++ test/unitTester.cpp
@@ -5,7 +5,7 @@
 
 TEST(UnitTest, ParseUnit)
 {
-    Monster expected("Fallen", 4, 1, 1, 1.6, 2, "");
+    Monster expected("Fallen", 4, 1, 1, 1.6, 2, "texture/fallen.png");
     Monster result = Monster::parse("../unit/Fallen.json");
     EXPECT_EQ(expected.getName(), result.getName());
     EXPECT_EQ(expected.getHealthPoints(), result.getHealthPoints());
@@ -13,11 +13,12 @@ TEST(UnitTest, ParseUnit)
     EXPECT_EQ(expected.getMagicalDamage(), result.getMagicalDamage());
     EXPECT_EQ(expected.getAttackCoolDown(), result.getAttackCoolDown());
     EXPECT_EQ(expected.getDefense(), result.getDefense());
+    EXPECT_EQ(expected.getTexture(), result.getTexture());
 }
 TEST(UnitTest, Minusheal)
 {
-    Monster alive("Blood Raven", 113, 5, 3, 1.2, 3, "");
+    Monster alive("Blood Raven", 113, 5, 3, 1.2, 3, "texture/blood_raven.png");
     EXPECT_EQ(alive.isAlive(), true);
-    Monster dead("Blood Raven", -1, 5, 3, 1.2, 3, "");
+    Monster dead("Blood Raven", -1, 5, 3, 1.2, 3, "texture/blood_raven.png");
     EXPECT_EQ(dead.isAlive(), false);
 }
